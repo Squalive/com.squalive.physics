14fed38ca9e4f4f60c32a38d2965629e50633652
CodingDaniel1 <cuberdanielchen@gmail.com>
2024-06-19 18:25:07 +0800

    First init

---
+++ Unity.Physics.Hybrid/Authoring/CollisionFilterAuthoring.cs
@@ -0,0 +1,55 @@
+﻿
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Unity.Physics.Hybrid.Authoring
+{
+    [AddComponentMenu("Physics/Collision Filter")]
+    public class CollisionFilterAuthoring : MonoBehaviour
+    {
+        public CollisionLayer belongsTo = CollisionLayer.Environment;
+
+        public CollisionLayer collidesWith = CollisionLayer.Everything;
+    }
+
+#if UNITY_EDITOR
+    [CustomEditor(typeof(CollisionFilterAuthoring))]
+    public class CollisionLayerEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var layer = (CollisionFilterAuthoring)target;
+
+            EditorGUI.BeginChangeCheck();
+
+            EditorGUILayout.BeginVertical("Box");
+
+            EditorGUI.indentLevel = 0;
+
+            EditorGUILayout.LabelField("Filter");
+
+            EditorGUI.indentLevel = 1;
+
+            GUILayout.Space(5);
+
+            layer.belongsTo = (CollisionLayer)EditorGUILayout.EnumFlagsField("Belongs To", layer.belongsTo);
+
+            layer.collidesWith = (CollisionLayer)EditorGUILayout.EnumFlagsField("Collides With", layer.collidesWith);
+
+            EditorGUILayout.EndVertical();
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                Undo.RecordObject(layer, "Changed Layers");
+
+                EditorUtility.SetDirty(layer);
+
+                serializedObject.ApplyModifiedProperties();
+            }
+        }
+    }
+#endif
+}
---
+++ Unity.Physics.Hybrid/CollisionLayer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Flags]
+public enum CollisionLayer
+{
+    None = 0,
+    Environment = 1 << 0,
+    Player = 1 << 1,
+    PhysicalItem = 1 << 2,
+    Workbench = 1 << 3,
+    PhysicalLoot = 1 << 4,
+    Trigger = 1 << 5,
+    Everything = ~0,
+}
